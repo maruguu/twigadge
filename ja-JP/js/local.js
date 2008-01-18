12bf8010fb2d41af785bac6109ba8c1e36172588
maruguu <maruguu@6cdbaa9e-fe43-0410-9b45-d17858d1eb4a>
2008-01-18 14:32:32 +0000

    confirmed working

--- ja-JP/js/local.js
+++ ja-JP/js/local.js
@@ -3,21 +3,21 @@
 //
 // Main gadget strings
 //
-LOCAL.send_finish = "メッセージ送信 : 完了";
-LOCAL.send_timeout = "メッセージ送信 : 時間切れ";
-LOCAL.send_message = "メッセージ送信 : 送信中";
+LOCAL.send_finish = "メッセージ送信完了";
+LOCAL.send_timeout = "メッセージ送信タイムアウト";
+LOCAL.send_message = "メッセージ送信中";
 LOCAL.send_cation = "このウィンドウが自動的に消えるまでお待ちください";
 LOCAL.send_cation2 = "処理中";
 
-LOCAL.recv_finish = "更新 : 完了";
-LOCAL.recv_timeout = "更新 : 時間切れ";
-LOCAL.recv_message = "更新 : 更新中";
-LOCAL.recv_message2 = "更新 : 更新中 ...";
+LOCAL.recv_finish = "更新完了";
+LOCAL.recv_timeout = "更新タイムアウト";
+LOCAL.recv_message = "更新中";
+LOCAL.recv_message2 = "更新中 ...";
 
-LOCAL.get_feed_start    = "Buzztter : 更新開始";
-LOCAL.get_feed_progress = "Buzztter : 更新中"
-LOCAL.get_feed_success  = "Buzztter : 更新完了";
-LOCAL.get_feed_error    = "Buzztter : エラー";
+LOCAL.get_feed_start    = "Buzztter更新開始";
+LOCAL.get_feed_progress = "Buzztter更新中"
+LOCAL.get_feed_success  = "Buzztter更新完了";
+LOCAL.get_feed_error    = "Buzztter更新エラー";
 
 
 //
