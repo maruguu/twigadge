fc7121dbfa571130f7ee687825449e49360dd679
maruguu <maruguu@6cdbaa9e-fe43-0410-9b45-d17858d1eb4a>
2008-01-19 08:43:38 +0000

    Change settings.html to use tab

--- trunk/js/settings.js
+++ trunk/js/settings.js
@@ -1,30 +1,84 @@
 /*
  *  settings.js
- *  (c)2007 maruguu
+ *  Copyright(c) 2007-2008 maruguu
  */
+
+//  temporary value for setting
+var temp_user;
+var temp_pass;
+var temp_interval;
+var temp_post;
+
+var previous_page;
+
+// UI Data
+var pages = new Array();
+pages[0] = '<p>ユーザ名:<br><input type="text" id="user" value="" class="input-box" /></p><p>パスワード:<br><input type="password" id="pass" value="" class="input-box" /></p><p>更新間隔(分):<br><input type="text" id="interval" value="" class="input-box" /></p><tr><td style="width:30%" style="padding-top:3px"><input type="checkbox" name="post" value="" />ステータスの更新にPOSTを使う</td></tr>';
+
+pages[1] = "仮置き1";
+pages[2] = "仮置き2";
 
 // Initialize
 function settingsInit() {
   window.detachEvent('onload', settingsInit);
   System.Gadget.onSettingsClosing = settingsClosing;
   
-  $('user').value = System.Gadget.Settings.read('username');
-  $('pass').value = System.Gadget.Settings.read('passwords');
-  $('interval').value = System.Gadget.Settings.read('interval');
-  if($('interval').value == '') $('interval').value = '5';
+  temp_user = System.Gadget.Settings.read('username');
+  temp_pass = System.Gadget.Settings.read('passwords');
+  temp_interval = System.Gadget.Settings.read('interval');
+  if(temp_interval == '') temp_interval = '5';
+  temp_post = (System.Gadget.Settings.read('post') == 'true');
+  
+  previous_page = 0;
+  showtab(0);
+}
+
+
+function saveSettings(page) {
+  if(page == 0) {
+    temp_user = $('user').value;
+    temp_pass = $('pass').value;
+    temp_interval = $('interval').value;
+    temp_post = $('post').checked;
+  }
 }
 
 // Closing
 function settingsClosing(event) {
   if(event.closeAction == event.Action.commit) {
-    var i = parseInt($('interval').value);
+    saveSettings(previous_page);
+    
+    var i = parseInt(temp_interval);
     if(isNaN(i) || i <= 0) {
       i = 5;
     }
-    System.Gadget.Settings.write('username', $('user').value);
-    System.Gadget.Settings.write('passwords', $('pass').value);
+    var p = (temp_post) ? 'true' : 'false'; 
+    System.Gadget.Settings.write('username', temp_user);
+    System.Gadget.Settings.write('passwords', temp_pass);
     System.Gadget.Settings.write('interval', i);
+    System.Gadget.Settings.write('post', p);
   }
+}
+
+// Settings UI
+function showtab(page) {
+  c = (page < 0) ? "" : pages[page];
+  
+  // Save settings
+  if(previous_page != page) {
+    saveSettings(previous_page);
+  }
+  
+  $('content').innerHTML = c;
+  if(page == 0) {
+    $('user').value = temp_user;
+    $('pass').value = temp_pass;
+    $('interval').value = temp_interval;
+    $('post').checked = temp_post;
+  }
+  
+  
+  previous_page = page;
 }
 
 window.attachEvent('onload', settingsInit);
