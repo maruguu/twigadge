3c113612b140dc050e848f5292a65b26c78c66cc
maruguu <maruguu@6cdbaa9e-fe43-0410-9b45-d17858d1eb4a>
2008-01-20 02:30:22 +0000

    Change settings.html Tab UI

--- js/settings.js
+++ js/settings.js
@@ -15,8 +15,13 @@ var previous_page;
 var pages = new Array();
 pages[0] = '<p>ユーザ名:<br><input type="text" id="user" value="" class="input-box" /></p><p>パスワード:<br><input type="password" id="pass" value="" class="input-box" /></p><p>更新間隔(分):<br><input type="text" id="interval" value="" class="input-box" /></p><tr><td style="width:30%" style="padding-top:3px"><input type="checkbox" name="post" value="" />ステータスの更新にPOSTを使う</td></tr>';
 
-pages[1] = "仮置き1";
-pages[2] = "仮置き2";
+pages[1] = '仮置き1';
+pages[2] = '仮置き2';
+
+var menu = new Array();
+menu[0] = '<a class="tab" onClick="showtab(0)">通信</a>';
+menu[1] = '<a class="tab" onClick="showtab(1)">表示</a>';
+menu[2] = '<a class="tab" onClick="showtab(2)">その他</a>';
 
 // Initialize
 function settingsInit() {
@@ -68,7 +73,20 @@ function showtab(page) {
   if(previous_page != page) {
     saveSettings(previous_page);
   }
+  // menu
+  $('menu').innerHTML = "";
+  for(i = 0; i < menu.length; i++) {
+    var m = menu[i];
+    if(i == page) {
+      m = m.replace("tab", "current_tab");
+    }
+    $('menu').innerHTML += m;
+    if(i < menu.length - 1) {
+      $('menu').innerHTML += " | ";
+    }
+  }
   
+  // content
   $('content').innerHTML = c;
   if(page == 0) {
     $('user').value = temp_user;
