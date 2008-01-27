e8572e1c5959520ae8801aa91318c588ec4765be
maruguu <maruguu@6cdbaa9e-fe43-0410-9b45-d17858d1eb4a>
2008-01-27 07:02:25 +0000

    Version 0.6 Release

---
+++ tags/ver0.6/js/flyout_comment.js
@@ -0,0 +1,24 @@
+function flyoutShowing() {
+  var sgf = System.Gadget.Flyout;
+  var sgs = System.Gadget.Settings;
+  var username = sgs.read('show_username');
+  if(username != '') {
+    $('username').innerHTML = '<a class="twitter" href="http://www.twitter.com/' + username+ '/">' + username + '</a>';
+  } else {
+    $('username').innerHTML = 'unknown';
+  }
+  var image = sgs.read('show_image');
+  if(image != '') {
+    $('profile-image').innerHTML = '<img src="' + image + '"/>';
+  } else {
+    $('profile-image').innerHTML = '<img alt="Not found" />';
+  }
+  var text = sgs.read('show_status');
+  if(text != '') {
+    $('comment').innerHTML = text;
+  } else {
+    $('comment').innerHTML = 'undefined';
+  }
+  
+  sgf.document.body.style.height = document.documentElement.scrollHeight;
+}
---
+++ tags/ver0.6/js/settings.js
@@ -0,0 +1,91 @@
+/*
+ *  settings.js
+ */
+var Settings = {};
+var us;
+var previous_page;
+
+// Initialize
+Settings.init = function() {
+  window.detachEvent('onload', Settings.init);
+  System.Gadget.onSettingsClosing = Settings.closing;
+  
+  us = new Twigadge.Settings();
+  us.read();
+  previous_page = 0;
+  Settings.showtab(0);
+}
+
+
+Settings.store = function(page) {
+  if(page == 0) {
+    us.username = $('user').value;
+    us.password = $('pass').value;
+    us.interval = $('interval').value;
+    us.post = $('post').checked;
+  } else if(page == 1) {
+    us.width = $('width').value;
+    us.height = $('height').value;
+    us.scroller = $('scroller').checked;
+    us.fixed = $('fixed').checked;
+  } else if(page == 2) {
+    us.buzztter.enable = $('buzz_enable').checked;
+    us.buzztter.interval = $('buzz_interval').value;
+  } else if(page == 3) {
+    us.check_ver = $('check_ver').checked;
+  }
+}
+
+// Closing
+Settings.closing = function(event) {
+  if(event.closeAction == event.Action.commit) {
+    Settings.store(previous_page);
+    us.write();
+  }
+}
+
+// Settings UI
+Settings.showtab = function(page) {
+  c = (page < 0) ? "" : pages[page];
+  
+  // Save settings
+  if(previous_page != page) {
+    Settings.store(previous_page);
+  }
+  // menu
+  $('menu').innerHTML = "";
+  for(i = 0; i < menu.length; i++) {
+    var m = menu[i];
+    if(i == page) {
+      m = m.replace("tab", "current_tab");
+    }
+    $('menu').innerHTML += m;
+    if(i < menu.length - 1) {
+      $('menu').innerHTML += " | ";
+    }
+  }
+  
+  // content
+  $('content').innerHTML = c;
+  if(page == 0) {
+    $('user').value = us.username;
+    $('pass').value = us.password;
+    $('interval').value = us.interval;
+    $('post').checked = us.post;
+  } else if(page == 1) {
+    $('width').value = us.width;
+    $('height').value = us.height;
+    $('scroller').checked = us.scroller;
+    $('fixed').checked = us.fixed;
+  } else if(page == 2) {
+    $('buzz_enable').checked = us.buzztter.enable;
+    $('buzz_interval').value = us.buzztter.interval;
+  } else if(page == 3) {
+    $('check_ver').checked = us.check_ver;
+    $('version').innerHTML = 'Version ' + System.Gadget.version;
+  }
+  previous_page = page;
+}
+
+window.attachEvent('onload', Settings.init);
+
