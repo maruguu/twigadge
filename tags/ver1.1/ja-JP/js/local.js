922ca1b38631c82efe231583d89f1505cf2ac7e3
maruguu <maruguu@6cdbaa9e-fe43-0410-9b45-d17858d1eb4a>
2008-08-10 12:33:19 +0000

    Version 1.1 Release

---
+++ tags/ver1.1/ja-JP/js/local.js
@@ -0,0 +1,55 @@
+/*
+ * local.js - l10n for gadget
+ */
+var Local = {};
+
+Local.gotoDLPage = "最新版が公開されています\n\nVersion %%GADGET_VERSION%% -> Version %%LATEST_VERSION%%\n\nダウンロードページを表示しますか?";
+Local.setUsername = "ユーザ情報未設定";
+Local.getFriendsTimeline = "タイムライン更新開始";
+Local.getFriendsTimeline2 = "タイムライン更新中 ...";
+Local.getFriendsTimelineTimeout = "タイムライン更新タイムアウト";
+Local.getFriendsTimelineFinished = "タイムライン更新完了";
+
+Local.getDirectMessage = "ダイレクトメッセージ取得開始";
+Local.getDirectMessage2 = "ダイレクトメッセージ取得中 ...";
+Local.getDirectMessageTimeout = "ダイレクトメッセージ取得タイムアウト";
+Local.getDirectMessageFinished = "ダイレクトメッセージ取得完了";
+
+Local.getReply = "返信取得開始";
+Local.getReply2 = "返信取得中 ...";
+Local.getReplyTimeout = "返信取得タイムアウト";
+Local.getReplyFinished = "返信取得完了";
+
+Local.buzztterUrl = "http://buzztter.com/ja/";
+Local.getBuzztterFeed = "Buzztterフィード取得開始";
+Local.getBuzztterFeed2 = "Buzztterフィード取得中 ...";
+Local.getBuzztterTimeout = "Buzztterフィード取得タイムアウト";
+Local.getBuzztterFinished = "Buzztterフィード取得完了";
+
+Local.error304 = "更新無し";
+Local.error400 = "API実行回数制限";
+Local.error401 = "認証失敗";
+Local.error403 = "実行権限無し"
+Local.error404 = "存在せず";
+Local.error500 = "Twitterに問題あり";
+Local.error502 = "Twitterのサーバが停止中";
+Local.error503 = "Twitterのサーバが高負荷状態";
+
+Local.messagesizeShort = "本文が短すぎます";
+Local.updateStatus = "投稿開始";
+Local.updateStatus2 = "投稿中 ...";
+Local.updateStatusTimeout = "投稿タイムアウト";
+Local.updateStatusFinished = "投稿完了";
+
+Local.favorite = "お気に入りに登録開始";
+Local.favorite2 = "お気に入りに登録中 ...";
+Local.favoriteTimeout = "お気に入りに登録タイムアウト";
+Local.favoriteFinished = "お気に入りに登録完了";
+
+Local.unfavorite = "お気に入りの解除開始";
+Local.unfavorite2 = "お気に入りの解除中 ...";
+Local.unfavoriteTimeout = "お気に入りの解除タイムアウト";
+Local.unfavoriteFinished = "お気に入りの解除完了";
+
+
+
---
+++ tags/ver1.1/js/sendmessage.js
@@ -0,0 +1,43 @@
+/*
+ * sendmessage.js
+ */
+
+var SendMessage = function() {
+  var maxLength = 140;
+  return {
+    updateStatus: function() {
+      var len = $('update-text').value.length;
+      if(len == 0) {
+        $('status').innerHTML = Local.messagesizeShort;
+        return ;
+      }
+      setTimeout('System.Gadget.document.parentWindow.Twigadge.updateStatus()', 1);
+    },
+    
+    updateLength: function() {
+      var str = $('update-text').value.length;
+      var len = parseInt(str, 10);
+      if(len > maxLength) {
+        str = '<font color="#ff0000">' + str + '</font>';
+      }
+      $('string-length').innerHTML = str + ' / ' + maxLength;
+      $('status').innerHTML = '';
+    },
+    
+    submitOnCtrlEnter: function() {
+      if(event.ctrlKey && event.keyCode == 13) {
+        SendMessage.updateStatus();
+      }
+    },
+    
+    pageLoad: function() {
+      window.detachEvent('onload', SendMessage.pageLoad);
+      
+      $('update-text').onkeydown = SendMessage.submitOnCtrlEnter;
+      $('update-text').onkeyup = SendMessage.updateLength;
+      $('update-button').onclick = SendMessage.updateStatus;
+    }
+  };
+}();
+
+window.attachEvent('onload', SendMessage.pageLoad);
