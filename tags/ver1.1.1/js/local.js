253bcb9d22b27f8257141fc09907ddfdfb46d723
maruguu <maruguu@6cdbaa9e-fe43-0410-9b45-d17858d1eb4a>
2009-05-11 11:21:41 +0000

    Version 1.1.1 Release

---
+++ tags/ver1.1.1/js/local.js
@@ -0,0 +1,55 @@
+/*
+ * local.js - l10n for gadget
+ */
+var Local = {};
+
+Local.gotoDLPage = "The latest version is available. \n\nVersion %%GADGET_VERSION%% -> Version %%LATEST_VERSION%%\n\nClicking OK will take you to download page";
+Local.setUsername = "set username";
+Local.getFriendsTimeline = "getting friends timeline";
+Local.getFriendsTimeline2 = "getting friends timeline ...";
+Local.getFriendsTimelineTimeout = "timeout - getting friends timeline";
+Local.getFriendsTimelineFinished = "finish - getting friends timeline";
+
+Local.getDirectMessage = "getting direct messages";
+Local.getDirectMessage2 = "getting direct messages ...";
+Local.getDirectMessageTimeout = "timeout - getting direct messages";
+Local.getDirectMessageFinished = "finish - getting direct messages";
+
+Local.getReply = "getting reply";
+Local.getReply2 = "getting reply ...";
+Local.getReplyTimeout = "timeout - getting reply";
+Local.getReplyFinished = "finish - getting reply";
+
+Local.buzztterUrl = "http://buzztter.com/en/";
+Local.getBuzztterFeed = "getting feed from Buzztter";
+Local.getBuzztterFeed2 = "getting feed from Buzztter ...";
+Local.getBuzztterTimeout = "timeout - getting feed from Buzztter";
+Local.getBuzztterFinished = "finished - getting feed from Buzztter";
+
+Local.error304 = "there was no new data to return.";
+Local.error400 = "you've exceeded the rate limit.";
+Local.error401 = "you need to provide authentication credentials";
+Local.error403 = "your request is forbidden."
+Local.error404 = "not found";
+Local.error500 = "Twitter is something wrong.";
+Local.error502 = "Twitter is down, or being upgraded.";
+Local.error503 = "the Twitter servers are up, but are overloaded with requests.  Try again later.";
+
+Local.messagesizeShort = "too short ...";
+Local.updateStatus = "updating your status";
+Local.updateStatus2 = "updating your status ...";
+Local.updateStatusTimeout = "timeout - updating your status";
+Local.updateStatusFinished = "finish - updating your status";
+
+Local.favorite = "favorite an update";
+Local.favorite2 = "favorite an update ...";
+Local.favoriteTimeout = "timeout - favorite an update";
+Local.favoriteFinished = "finish - favorite an update";
+
+Local.unfavorite = "unfavorite an update";
+Local.unfavorite2 = "unfavorite an update ...";
+Local.unfavoriteTimeout = "timeout - unfavorite an update";
+Local.unfavoriteFinished = "finish - unfavorite an update";
+
+
+
