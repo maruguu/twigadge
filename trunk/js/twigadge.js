e8aab8b338fcb9a1aa280ae8df80ef9221a2f29b
maruguu <maruguu@6cdbaa9e-fe43-0410-9b45-d17858d1eb4a>
2009-07-08 08:02:49 +0000

    Add hashtags convertion to twitter search.

--- trunk/js/twigadge.js
+++ trunk/js/twigadge.js
@@ -171,8 +171,8 @@ var Twigadge = function() {
     
     var httpURL = /(s?https?:\/\/[-_.!~*'()a-zA-Z0-9;\/?:\@&=+\$,%#]+)/g;  //'
     var body = twit.text.replace(httpURL, '<a href="$1">$1</a>');
-    var hashtag = /\s*\#[a-zA-Z0-9]*[\s\r]*/g;
-    body = body.replace(hashtag, '<a href="http://search.twitter.com/search?q=$1">$1</a>');
+    var hashtags = /\s*\#[a-zA-Z0-9]*[\s\r]*/g;
+    body = body.replace(hashtags, '<a href="http://search.twitter.com/search?q=$1">$1</a>');
     // highlight buzzword (from buzztter)
     if(settings.buzztter.enable) {
       body = Buzztter.replace(body);
